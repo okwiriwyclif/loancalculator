3026572e765d718ca600255612afe917c9ba1d9e
okwiriwyclif <okwiriwyclif@gmail.com>
2022-02-23 15:12:59 +0300

    design update

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="  py-12 ">
+  <div >
 
     <div>
 
-      <div class="logo flex justify-center p-2">
+      <!-- <div class="logo flex justify-center p-2">
         <a href="https://www.nacicosacco.coop/">
         <img src="https://uploads-ssl.webflow.com/5e58cda44172e816058a6f60/5e58ce13f68ab83428a8a4ae_logo1.png" alt="Logo">
         </a>
-      </div>
+      </div> -->
 
-    <div class="flex justify-center">
+    <div >
       <client-only>
 
         <calculator/>
